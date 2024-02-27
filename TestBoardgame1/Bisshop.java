6914803cd198ad87aaa95c622c9e65ef509c9dff
cmosnato <cmoscmark@gmail.com>
2024-02-27 20:17:05 +0700

    kuy bishop 2

--- TestBoardgame1/Bisshop.java
+++ TestBoardgame1/Bisshop.java
@@ -9,46 +9,40 @@ public class Bisshop extends Figure {
 
     @Override
     void Move(Chessboard b, String position) {
-        String checkstring = this.name.substring(0, 1);
         int moveRow = (position.charAt(1) - '0') - 1;
         int moveColumn = position.charAt(0) - 'a';
         int currentRow = (this.getCurrentposition().charAt(1) - '0') - 1;
         int currentColumn = this.getCurrentposition().charAt(0) - 'a';
-        if (moveRow < 0 && moveRow >= b.table && moveColumn < 0 && moveColumn > b.table) {
+        if (moveRow <0 && moveRow >= b.table && moveColumn <0 && moveColumn > b.table) {
             System.out.println("Move position is out of board range.");
             return;
         } else if (Math.abs(moveRow - currentRow) != Math.abs(moveColumn - currentColumn)) {
             System.out.println(
                     "Cannot move " + this.getName() + " to " + position + " because it can only move diagonally.");
             return;
-        } else {
-            if (b.board[moveRow][moveColumn] == null
-                    || !b.board[moveRow][moveColumn].getName().substring(0, 1).equals(checkstring)) {
-                int rowDirection = (moveRow - currentRow > 0) ? 1 : -1;
-                int columnDirection = (moveColumn - currentColumn > 0) ? 1 : -1;
-                int checkRow = currentRow + rowDirection;
-                int checkColumn = currentColumn + columnDirection;
-                while (checkRow != moveRow && checkColumn != moveColumn && checkColumn > 0 && checkRow > 0) {
-                    if (b.board[checkRow][checkColumn] != null
-                            && b.board[checkRow][checkColumn].isWhite() == this.isWhite) {
-                        System.out.println("Cannot move " + this.getName() + " to " + position + " because the path "
-                                + b.board[checkRow][checkColumn].name + " is blocked.");
-                        return;
-                    }
-                    if (b.board[checkRow][checkColumn] != null) {
-                        System.out.println(
-                                "Cannot move " + this.getName() + " to " + position + " because the path is blocked.");
-                        return;
-                    }
-                    checkRow += rowDirection;
-                    checkColumn += columnDirection;
+        } else {if (b.board[moveRow][moveColumn]==null||b.board[moveRow][moveColumn].isWhite() != this.isWhite()) {
+            int rowDirection = (moveRow - currentRow > 0) ? 1 : -1;
+            int columnDirection = (moveColumn - currentColumn > 0) ? 1 : -1;
+            int checkRow = currentRow + rowDirection;
+            int checkColumn = currentColumn + columnDirection;
+            while (checkRow != moveRow && checkColumn != moveColumn && checkColumn>0&&checkRow>0) {
+                if (b.board[checkRow][checkColumn] != null && b.board[checkRow][checkColumn].isWhite()==this.isWhite) {
+                    System.out.println("Cannot move " + this.getName() + " to " + position + " because the path "+b.board[checkRow][checkColumn].name+ " is blocked.");
+                    return;
                 }
-                b.board[currentRow][currentColumn] = null;
-                b.board[moveRow][moveColumn] = this;
-                this.setCurrentposition(position);
-                System.out.println("Moved " + this.name + " To " + position);
-
+                if (b.board[checkRow][checkColumn] != null ) {
+                    System.out.println("Cannot move " + this.getName() + " to " + position + " because the path is blocked.");
+                    return;
+                }
+                checkRow += rowDirection;
+                checkColumn += columnDirection;
             }
+            b.board[currentRow][currentColumn] = null;
+            b.board[moveRow][moveColumn] = this;
+            this.setCurrentposition(position);
+            System.out.println("Moved " + this.name + " To " + position);
+            
+        }
         }
     }
 
